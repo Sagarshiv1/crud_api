ebc655a499759ded06ae4b3291f0287e0c6ae12d
shiv <sagar.shivshankar@thinksys.com>
2022-04-12 14:08:31 +0530

    changes

--- Controllers/users.controller.js
+++ Controllers/users.controller.js
@@ -1,17 +1,29 @@
 const express = require("express")
 const pool = require("../configues/db")
-const createUser = async(req, res) => {
-    const {first_name,last_name} = req.body
-    try{
-  
-   const user = await pool.query('INSERT INTO users (first_name,last_name) VALUES ($1,$2)', [first_name,last_name])
-      return res.status(201).send(user)
-    }catch(e){
-      return res.status(500).send("Something went wrong")
-    }
+const router = express.Router()
 
-  }
-  const getUsers = async(req, res) => {
+const { body, validationResult } = require('express-validator');
+
+router.post("",body('first_name').isLength({ min: 1 }).withMessage("firstName is required"),
+      body('last_name').isLength({ min: 1 }).withMessage("lastName is required")
+, async(req, res) => {
+    const {first_name,last_name} = req.body
+    const errors = validationResult(req);
+    
+    
+   if(!errors.isEmpty()){
+    return res.status(500).json({ errors: errors.array() });
+   
+   }
+    const users = await pool.query('INSERT INTO users (first_name,last_name) VALUES ($1,$2)', [first_name,last_name])
+    return res.status(201).json(users)
+   
+    
+   
+    
+
+  })
+  router.get("",async(req, res) => {
     // const {first_name,last_name} = req.body
     try{
    const users = await pool.query('select * from users')
@@ -19,9 +31,9 @@ const createUser = async(req, res) => {
     }catch(e){
       return res.status(500).send("Something went wrong")
     }
-  }
+  })
 
-  const getUsersById = async(req, res) => {
+  router.get("",async(req, res) => {
     const {id} = req.params
     try{
   
@@ -30,9 +42,9 @@ const createUser = async(req, res) => {
     }catch(e){
       return res.status(500).send("Something went wrong")
     }
-  }
+  })
 
-  const updateUser = async(req, res) => {
+  router.put("",async(req, res) => {
     const {id} = req.params
     const {first_name,last_name } = req.body
       try{
@@ -44,9 +56,9 @@ const createUser = async(req, res) => {
         return res.status(500).send("Something went wrong")
       }
     
-  }
+  })
   
-  const deleteUser = async (req, res) => {
+  router.delete("",async (req, res) => {
     const {id} = req.params
   try{
    const user = await pool.query('DELETE FROM users WHERE id = $1', [id])
@@ -54,5 +66,7 @@ const createUser = async(req, res) => {
     }catch(e){
       return res.status(500).send("Something went wrong")
     }
-  }
-  module.exports = {createUser,getUsers,getUsersById,updateUser,deleteUser}
+  })
+  module.exports = router
+  // module.exports = {getUsers,getUsersById,updateUser,deleteUser}
+  
